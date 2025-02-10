4894389300a27465bc4456992f773c71383fab0a
Denys Kaminskyi <135739796+dionisiyKDO@users.noreply.github.com>
2025-02-10 18:04:54 +0000

    Auto-update frontend build

---
+++ static/frontend/_app/immutable/chunks/CLse20bT.js
@@ -0,0 +1,1 @@
+import{V as o,p as t,W as c,X as u}from"./C-rDJMBQ.js";function l(n){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function r(n){t===null&&l(),c&&t.l!==null?a(t).m.push(n):o(()=>{const e=u(n);if(typeof e=="function")return e})}function a(n){var e=n.l;return e.u??(e.u={a:[],b:[],m:[]})}export{r as o};
---
+++ static/frontend/_app/immutable/entry/start.C14LU-ch.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/CasFrNk2.js";export{t as start};
